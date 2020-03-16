74fc6b7034f04a44c535424de173eb5aca1cda7f
sehee0531 <wjdtpgml0531@gmail.com>
2020-03-17 01:21:20 +0900

    turtle 창띄우기

---
+++ turtle 창띄우기.py
@@ -0,0 +1,18 @@
+import turtle
+
+t=turtle.Turtle()
+t.shape("turtle")
+
+turtle.textinput("입력창의 제목","설명라인")
+t.write("안녕하세요?홍길동씨, 터틀 인사드립니다.")
+
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+turtle.textinput("")
+turtle.done()
