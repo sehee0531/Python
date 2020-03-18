3f024ba76741cfb8624e461521f2fbc05b14c53a
sehee0531 <wjdtpgml0531@gmail.com>
2020-03-18 18:45:10 +0900

    turtle 두점 사이의 거리계산

---
+++ turtle 두 점 사이의 거리계산.py
@@ -0,0 +1,20 @@
+import turtle
+
+t=turtle.Turtle()
+t.shape("turtle")
+
+
+x1=float(input("x1 :"))
+y1=float(input("y1 :"))
+x2=float(input("x2 :"))
+y2=float(input("y2 :"))
+
+
+t.goto(x1,y1)
+t.left(45)
+t.forward(((x1-x2)**2+(y1-y2)**2)**0.5)
+t.goto(x2,x1)
+t.goto(x1,y1)
+
+
+turtle.done()
