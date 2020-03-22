93e7704df45948b6d0ab1d6b110dca5311fd7fa3
sehee0531 <wjdtpgml0531@gmail.com>
2020-03-22 23:26:57 +0900

    turtle 이동과제3

---
+++ 정세희_과제1.py
@@ -0,0 +1,15 @@
+import turtle
+
+t=turtle.Turtle()
+t.shape("turtle")
+
+t.left(60)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(200)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
