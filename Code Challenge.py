8c577360b9e21454fc3d155d13864c5bef88d5a2
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2021-01-11 22:30:02 +0900

    (New)Code Challenge

---
+++ Code Challenge.py
@@ -0,0 +1,50 @@
+def plus(a,b):
+    a=int(a)
+    b=int(b)
+    return a+b
+
+print(plus(1,"2"))
+
+def minus(a,b):
+    a=int(a)
+    b=int(b)
+    return a-b
+
+print(minus("1","2"))
+
+def time(a,b):
+    a=int(a)
+    b=int(b)
+    return a*b
+
+print(time(1,2))
+
+def divd(a,b):
+    a=int(a)
+    b=int(b)
+    return a/b
+
+print(divd(4,"2"))
+
+def neg(a):
+    a=int(a)
+    
+    return -a
+
+print(neg("1"))
+
+def power(a,b):
+    a=int(a)
+    b=int(b)
+    return a**b
+
+print(power(1,"2"))
+
+def remainder(a,b):
+    a=int(a)
+    b=int(b)
+    return a%b
+
+print(remainder(1,"2"))
+
+
