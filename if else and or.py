6f6a1c7056148f689139d1f4bee7d07592a299ec
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2021-01-14 23:51:02 +0900

    (New)If else and or

---
+++ if else and or.py
@@ -0,0 +1,14 @@
+def age_check(age):
+    print(f"you are {age}")
+    if age<18:
+        print("you cant drink")
+    elif age==18 or age==19:
+        print("you are new to this")
+    elif age >20 and age <25:
+        print("you are still kind of young")
+    else:
+        print("enjoy your drink")
+
+age_check(19)
+age_check(20)
+age_check(24)
